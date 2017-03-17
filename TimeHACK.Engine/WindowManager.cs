0c00b5f33c37427228c53794732d513adc862261
AShifter <loganr.lowe@gmail.com>
2017-03-17 17:31:41 -0600

    HUGE OVERHAUL
    Added TimeHACK.Engine - Starting a new engine system to render Operating
    System borders
    
    Made more changes ( ͡° ͜ʖ ͡°)

---
+++ TimeHACK.Engine/WindowManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeHACK.Engine
+{
+    class WindowManager
+    {
+        private void startWinClassic(String title, int width, int height)
+        {
+
+        }
+    }
+}
---
+++ TimeHACK.Main/WinClassicForms/TestApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeHACK.Engine.Template;
+using System.Windows.Forms;
+
+namespace TimeHACK.WinClassicForms
+{
+    public partial class TestApp : UserControl
+    {
+        public TestApp()
+        {
+            InitializeComponent();
+        }
+
+
+
+    }
+}
+
