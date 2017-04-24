8dd3326bd247b8e31fc16d9ed89a23a2b358f9bf
AShifter <loganr.lowe@gmail.com>
2017-04-24 08:25:57 -0600

    Added WinClassicHWCV
    and some other stuff i think

---
+++ TimeHACK.Main/OS/Win95/Win95Apps/WinClassicHWCV.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace TimeHACK.OS.Win95.Win95Apps
+{
+    partial class WinClassicHWCV
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WinClassicHWCV));
+            this.program = new System.Windows.Forms.Panel();
+            this.Panel2 = new System.Windows.Forms.Panel();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.toprightcorner = new System.Windows.Forms.Panel();
+            this.bottomrightcorner = new System.Windows.Forms.Panel();
+            this.bottomleftcorner = new System.Windows.Forms.Panel();
+            this.topleftcorner = new System.Windows.Forms.Panel();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.program.SuspendLayout();
+            this.Panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // program
+            // 
+            this.program.BackColor = System.Drawing.Color.Silver;
+            this.program.Controls.Add(this.PictureBox1);
+            this.program.Controls.Add(this.Panel2);
+            this.program.Controls.Add(this.toprightcorner);
+            this.program.Controls.Add(this.bottomrightcorner);
+            this.program.Controls.Add(this.bottomleftcorner);
+            this.program.Controls.Add(this.topleftcorner);
+            this.program.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.program.Location = new System.Drawing.Point(0, 0);
+            this.program.Name = "program";
+            this.program.Size = new System.Drawing.Size(361, 324);
+            this.program.TabIndex = 11;
+            // 
+            // Panel2
+            // 
+            this.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Panel2.Controls.Add(this.Label3);
+            this.Panel2.Controls.Add(this.Button1);
+            this.Panel2.Controls.Add(this.TextBox2);
+            this.Panel2.Controls.Add(this.TextBox1);
+            this.Panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Panel2.Location = new System.Drawing.Point(0, 135);
+            this.Panel2.Name = "Panel2";
+            this.Panel2.Size = new System.Drawing.Size(361, 189);
+            this.Panel2.TabIndex = 9;
+            // 
+            // Label3
+            // 
+            this.Label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(7, 168);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(148, 13);
+            this.Label3.TabIndex = 3;
+            this.Label3.Text = "Content Found: Time Distortor";
+            // 
+            // Button1
+            // 
+            this.Button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button1.Location = new System.Drawing.Point(278, 145);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 20);
+            this.Button1.TabIndex = 2;
+            this.Button1.Text = "Display";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.TextBox2.Location = new System.Drawing.Point(12, 144);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(226, 20);
+            this.TextBox2.TabIndex = 1;
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TextBox1.BackColor = System.Drawing.Color.Silver;
+            this.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBox1.Location = new System.Drawing.Point(22, 4);
+            this.TextBox1.Multiline = true;
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.ReadOnly = true;
+            this.TextBox1.Size = new System.Drawing.Size(321, 137);
+            this.TextBox1.TabIndex = 0;
+            this.TextBox1.Text = resources.GetString("TextBox1.Text");
+            this.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // toprightcorner
+            // 
+            this.toprightcorner.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.toprightcorner.Location = new System.Drawing.Point(357, 0);
+            this.toprightcorner.Name = "toprightcorner";
+            this.toprightcorner.Size = new System.Drawing.Size(4, 4);
+            this.toprightcorner.TabIndex = 6;
+            // 
+            // bottomrightcorner
+            // 
+            this.bottomrightcorner.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bottomrightcorner.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.bottomrightcorner.Location = new System.Drawing.Point(357, 320);
+            this.bottomrightcorner.Name = "bottomrightcorner";
+            this.bottomrightcorner.Size = new System.Drawing.Size(4, 4);
+            this.bottomrightcorner.TabIndex = 4;
+            // 
+            // bottomleftcorner
+            // 
+            this.bottomleftcorner.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bottomleftcorner.Location = new System.Drawing.Point(0, 320);
+            this.bottomleftcorner.Name = "bottomleftcorner";
+            this.bottomleftcorner.Size = new System.Drawing.Size(4, 4);
+            this.bottomleftcorner.TabIndex = 2;
+            // 
+            // topleftcorner
+            // 
+            this.topleftcorner.Location = new System.Drawing.Point(0, 0);
+            this.topleftcorner.Name = "topleftcorner";
+            this.topleftcorner.Size = new System.Drawing.Size(4, 4);
+            this.topleftcorner.TabIndex = 1;
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PictureBox1.Image = global::TimeHACK.Properties.Resources.WinClassicHWCV;
+            this.PictureBox1.Location = new System.Drawing.Point(5, 24);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(351, 107);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox1.TabIndex = 0;
+            this.PictureBox1.TabStop = false;
+            // 
+            // WinClassicHWCV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.program);
+            this.Name = "WinClassicHWCV";
+            this.Size = new System.Drawing.Size(361, 324);
+            this.program.ResumeLayout(false);
+            this.Panel2.ResumeLayout(false);
+            this.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Panel program;
+        internal System.Windows.Forms.PictureBox PictureBox1;
+        internal System.Windows.Forms.Panel Panel2;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.TextBox TextBox2;
+        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.Panel toprightcorner;
+        internal System.Windows.Forms.Panel bottomrightcorner;
+        internal System.Windows.Forms.Panel bottomleftcorner;
+        internal System.Windows.Forms.Panel topleftcorner;
+    }
+}
