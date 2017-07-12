a52cfa8adcf24e9698adfbc243cd7e6a45793c1b
lempamo <smartypantslf@gmail.com>
2017-07-12 17:23:10 -0500

    Added TempIE4's start page

---
+++ TimeHACK.Main/OS/Win95/Win95Apps/IE4Sites/IE4Start.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeHACK.OS.Win95.Win95Apps.IE4Sites
+{
+    public partial class IE4Start : UserControl
+    {
+        public IE4Start()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_Click(object sender, EventArgs e)
+        {
+            TempIE4.GoToPage("www.google.com");
+        }
+    }
+}
--- TimeHACK.Main/OS/Win95/Win95Apps/TempIE4.cs
+++ TimeHACK.Main/OS/Win95/Win95Apps/TempIE4.cs
@@ -14,11 +14,13 @@ namespace TimeHACK.OS.Win95.Win95Apps
     public partial class TempIE4 : UserControl
     {
         public static Panel browsingArea = new Panel();
+        public static ComboBox addressbar = new ComboBox();
 
         public TempIE4()
         {
             InitializeComponent();
             browsingArea = _browsingArea;
+            addressbar = _addressbar;
         }
 
         public static void GoToPage(string url)
@@ -27,6 +29,9 @@ namespace TimeHACK.OS.Win95.Win95Apps
 
             switch (url)
             {
+                case "www.microsoft.com/windows/ie/default.htm":
+                    uc = new IE4Start();
+                    break;
                 case "www.google.com":
                     uc = new GoogleHome();
                     break;
@@ -35,6 +40,7 @@ namespace TimeHACK.OS.Win95.Win95Apps
                     break;
             }
 
+            addressbar.Text = url;
             uc.Dock = DockStyle.Fill;
             browsingArea.Controls.Clear();
             browsingArea.Controls.Add(uc);
@@ -42,7 +48,7 @@ namespace TimeHACK.OS.Win95.Win95Apps
 
         private void TempIE4_Load(object sender, EventArgs e)
         {
-            GoToPage("www.google.com");
+            GoToPage("www.microsoft.com/windows/ie/default.htm");
         }
 
         private void GoButton_Click(object sender, EventArgs e)
